cba43e5fd82aa0eeec0107964e00c18ac202c002
Luiz Guilherme <luizgmelo64@gmail.com>
2023-11-05 18:38:21 -0300

    add new knowledge javascript

--- js/script.js
+++ js/script.js
@@ -13,3 +13,7 @@ function checkWindow() {
         menu.style.display = "none"
     }
 }
+
+function sayHello() {
+    document.getElementById('test-p').innerHTML = 'Hello, You interact with Javascript! &#128515;'
+}

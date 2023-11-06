b3e4690abd32ba2d4f06d921157cc384a1fbeed5
Luiz Guilherme <luizgmelo64@gmail.com>
2023-11-05 23:36:30 -0300

    remove unnecessary menu

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-function changeMenu() {
-    if (menu.style.display == "none") {
-        menu.style.display = "block"
-    } else {
-        menu.style.display = "none"
-    }
-}
-
-function checkWindow() {
-    if (window.innerWidth >= 768) {
-        menu.style.display = "inline-block"
-    } else {
-        menu.style.display = "none"
-    }
-}
-
 function sayHello() {
     document.getElementById('test-p').innerHTML = 'Hello, You interact with Javascript! &#128515;'
 }
